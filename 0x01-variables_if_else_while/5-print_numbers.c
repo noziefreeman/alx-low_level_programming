11d9250140d89d9ef2b22b9cf275c0ee6fd36487
Nozie Freeman <noziefreeman@gmail.com>
2022-12-12 14:59:19 -0800

    5print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all single digit number of base 10
+ * starting from 0, followed by new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '0')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
